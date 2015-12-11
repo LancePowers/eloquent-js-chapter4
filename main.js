229bd9c513f2e523133bc2eba383a1d54f573819
Lance <Lance@thelpstory.com>
2015-12-11 07:51:25 -0700

    cleanup

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// sum range Create 2 functions to generate an array between to arguments and sum them.
+// sum range // -- Create 2 functions to generate an array between to arguments and sum them.
 function sum(range){
   var total = 0;
   for (var i = 0; i < range.length; i++){
@@ -25,3 +25,5 @@ console.log(sum(range(1, 10)));
 // → 55
 console.log(range(5, 2, -1));
 // → [5, 4, 3, 2]
+
+//
---
+++ readme.md
@@ -0,0 +1,8 @@
+# h1
+## h2
+
+*list
+* list
+* list
+### heading
+[link] 'string'
